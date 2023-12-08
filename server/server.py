7047c92c80d753a1ac743ec61039fdaa5d387af5
roboticist2 <126433879+roboticist2@users.noreply.github.com>
2023-12-08 18:21:39 +0900

    23.12.08

--- server/server.py
+++ server/server.py
@@ -6,51 +6,109 @@ import time
 import numpy as np
 import cv2
 import threading
+from pynput import keyboard
 
 model = load_model("keras_model.h5", compile=False)
 shared_image = None
 keyValue = -1
-
-# Start a socket listening for connections on 0.0.0.0:8000
-# (0.0.0.0 means all interfaces)
+keyValue2 = "0"
 server_socket = socket.socket()
 print("Socket initializing...")
 
-server_socket.bind(('0.0.0.0', 8000))
-server_socket.listen(0)
-print("Waiting for connection...")
+#%% keyboard flags
+now = 0
+go_flag = 0
+left_flag = 0
+right_flag = 0
+back_flag = 0
+exit_flag = 0
 
-server_socket, addr = server_socket.accept()
-video_stream = server_socket.makefile('rb')
-print("Connection established")
+current_pressed = set()
 
-def send_dirct(server_socket):
-    global shared_image
-    global keyValue
-    carState = -1
+def getkeyboard():
+    with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
+        listener.join()
+
+def on_press(key):
+
+    global go_flag, left_flag, right_flag, back_flag, exit_flag
+    current_pressed.add(key)
+    # print('Key %s pressed' % current_pressed)
+    try:
+        if key == keyboard.Key.esc:
+            print('Exit is pressed')
+            exit_flag=1
+        if key.char == 'i':
+            # print('go is pressed')
+            go_flag=1
+        if key.char == 'j':
+            # print('left is pressed')
+            left_flag=1
+        if key.char == 'l':
+            # print('right is pressed')
+            right_flag=1
+        if key.char == 'k':
+            # print('right is pressed')
+            back_flag=1
+
+    except:
+        pass
+
+# 키보드 뗄 때 실행
+def on_release(key):
+    global go_flag, left_flag, right_flag, back_flag, exit_flag
+    # print('Key %s released' %key)
+    # if key == keyboard.Key.esc:
+    #     return False
+    if key in current_pressed:
+        current_pressed.remove(key)
+
+    try:
+        if key.char == 'i':
+            # print('go is released')
+            go_flag=0
+        if key.char == 'j':
+            # print('left is released')
+            left_flag=0
+        if key.char == 'l':
+            # print('right is released')
+            right_flag=0
+        if key.char == 'k':
+            # print('right is pressed')
+            back_flag=0
+    except:
+        pass
+
+def start_server():
+    HOST = '0.0.0.0'
+    PORT = 8000
+
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind((HOST, PORT))
+    server_socket.listen(1)
+    print('Server is listening...')
 
     while True:
-        if keyValue == 50:
-            carState = 1
-        if keyValue == 49:
-            carState = 0
-        if shared_image is None:
-            dirct=3
-        else:
-            prediction = model.predict(shared_image, verbose=0)
-            dirct = np.argmax(prediction[0])
-            print(prediction, dirct)
-        server_socket.send(struct.pack('ii', carState , dirct))
+        print('Waiting for a connection...')
+        connection, addr = server_socket.accept()
+        print('Connection established with', addr)
+        video_stream = connection.makefile('rb')
+        client_thread = threading.Thread(target=handle_client_connection, args=(video_stream, addr))
+        client_thread.start()
 
-def receive_video(video_stream):
+        send_thread = threading.Thread(target=send_dirct, args=(connection,))
+        send_thread.start()
+
+        getkey_thread = threading.Thread(target=getkeyboard)
+        getkey_thread.start()
+
+def handle_client_connection(video_stream, addr):
     global shared_image
     global keyValue
     try:
-        start=time.time()
+        print("Connection established ")
+        start = time.time()
         while True:
-
-            #print('time:', round(time.time() - start, 1))
-
             image_len_data = video_stream.read(4)
             if not image_len_data:
                 print('Image loading fail')
@@ -84,22 +142,36 @@ def receive_video(video_stream):
             # 'q' 키를 누르면 종료
             keyValue = cv2.waitKey(1)
             if keyValue & 0xFF == ord('q'):
+                print('User quit')
                 server_socket.close()
                 cv2.destroyAllWindows()
                 break
 
-    finally:
-        server_socket.close()
+    except (ConnectionResetError, ConnectionAbortedError):
+        print('Connection closed with', addr)
+        video_stream.close()
         cv2.destroyAllWindows()
+def send_dirct(connection):
+    global shared_image
+    global go_flag, back_flag,left_flag,right_flag
 
-dirct_thread = threading.Thread(target=send_dirct, args=(server_socket,))
-dirct_thread.start()
+    while True:
+        try:
+            if shared_image is None:
+                dirct = 3
+            else:
+                prediction = model.predict(shared_image, verbose=0)
+                dirct = np.argmax(prediction[0])
+                #dirct = 1
 
-video_thread = threading.Thread(target=receive_video, args=(video_stream,))
-video_thread.start()
-print('video streaming start')
-video_thread.join()
-dirct_thread.join()
+            connection.send(struct.pack('iiii', go_flag, back_flag, left_flag, right_flag))
 
-server_socket.close()
-cv2.destroyAllWindows()
+        except ConnectionResetError:
+            while True:
+                try:
+                    None
+                except ConnectionResetError:
+                    pass  # 다시 연결되기를 대기
+
+if __name__ == "__main__":
+    start_server()
